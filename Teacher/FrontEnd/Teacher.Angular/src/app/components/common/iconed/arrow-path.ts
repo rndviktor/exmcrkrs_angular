be0da7668a7491f1b8429674075cc05afe6118eb
Viktar Kameniukou <vkameniukou@seatgeek.com>
2025-07-22 22:49:13 +0200

    fix: padding and margin + 2 icon elements

---
+++ Teacher/FrontEnd/Teacher.Angular/src/app/components/common/iconed/arrow-path.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  template: `<button
+    class="inline-flex items-center px-0.5 mx-0.5 py-1.5 bg-indigo-200 disabled:bg-indigo-100  hover:bg-indigo-400 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    aria-label="Edit"
+    [disabled]="disabled"
+    type="button"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    </svg>
+  </button>`
+})
+export class CheckIconComponent {
+  @Input() disabled: boolean = false
+}
---
+++ Teacher/FrontEnd/Teacher.Angular/src/app/components/common/iconed/checkbox.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  imports: [
+    FormsModule
+  ],
+  template: `
+    <div class="flex items-center space-x-2">
+      <input id="{{id || 'chbId'}}"
+        type="checkbox"
+        [(ngModel)]="checked"
+        [disabled]="disabled"
+        class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-500"
+      />
+      <label for="{{id || 'chbId'}}" class="text-gray-900 select-none">
+        Is Correct?
+      </label>
+    </div>`
+})
+export class CheckboxComponent {
+  @Input() checked: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() id: string|number|null|undefined;
+}
