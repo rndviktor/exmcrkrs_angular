01864c3537d71ae0f8db54771614c7f3c8da75e6
Viktar Kameniukou <vkameniukou@seatgeek.com>
2025-07-23 20:32:52 +0200

    fix: auto resize height of textarea regarding the context

--- Teacher/FrontEnd/Teacher.Angular/src/app/components/write/questionEdit/contentEditorForm.ts
+++ Teacher/FrontEnd/Teacher.Angular/src/app/components/write/questionEdit/contentEditorForm.ts
@@ -1,5 +1,13 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  Component, ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-content-editor-form',
@@ -7,25 +15,38 @@ import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, V
   template: `<div class="flex flex-col p-8 ">
     <form class="flex flex-col" [formGroup]="form" (ngSubmit)="onSubmit()" >
       <label for="content">Content:</label>
-      <textarea id="content" type="text" formControlName="content"></textarea>
+      <textarea #contentDiv (input)="autoResize($event)" id="content" type="text" formControlName="content" [style.height.px]="dynamicHeight"></textarea>
       <button type="submit" [disabled]="form.pristine || form.invalid" class="bg-indigo-200 hover:bg-indigo-400 disabled:bg-indigo-50 flex-none shadow-xl" >{{this.isEditMode ? 'Update' : 'Create'}}</button>
     </form>
   </div>`,
 })
-export class ContentEditorFormComponent implements OnChanges{
+export class ContentEditorFormComponent implements OnChanges {
+  @ViewChild('contentDiv') contentDiv!: ElementRef<HTMLDivElement>;
   form: FormGroup;
   @Input() content: any;
   @Input() isEditMode = false;
+  dynamicHeight = 0;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       content: ['', Validators.required],
-    })
+    });
+    this.dynamicHeight = 50;
+  }
+
+  updateHeight() {
+    if (this.contentDiv) {
+      setTimeout(() => {
+        this.dynamicHeight = this.contentDiv.nativeElement.scrollHeight;
+        console.log('init', this.dynamicHeight);
+      }, 0);
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['content'] && this.content) {
       this.form.patchValue({content: this.content});
+      this.updateHeight();
     }
   }
 
@@ -33,5 +54,11 @@ export class ContentEditorFormComponent implements OnChanges{
 
   onSubmit() {
     this.submitForm.emit({ content: this.form.value.content });
+  }
+
+  autoResize(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = 'auto'; // reset height
+    textarea.style.height = `${textarea.scrollHeight}px`; // set height based on scrollHeight
   }
 }
