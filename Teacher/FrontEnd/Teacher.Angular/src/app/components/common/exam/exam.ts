99b0d3f350e830ef38d8a417e2c70ebdb2d6fe35
Viktar Kameniukou <vkameniukou@seatgeek.com>
2025-09-23 12:10:26 +0200

    fix: markup for tests

--- Teacher/FrontEnd/Teacher.Angular/src/app/components/common/exam/exam.ts
+++ Teacher/FrontEnd/Teacher.Angular/src/app/components/common/exam/exam.ts
@@ -60,7 +60,7 @@ import {AccessCodeEdit} from '../../write/access-code-edit/access-code-edit';
           <app-title-edit [exam]="exam" (discardCalled)="handleDiscardTitleEdit()"/>
         }
       </div>
-      @if (exam.questions.length > 0) {
+      @if (exam?.questions?.length) {
         <div class="flex flex-row justify-between bg-indigo-200">
           <app-access-code-edit [exam]="exam" />
         </div>
