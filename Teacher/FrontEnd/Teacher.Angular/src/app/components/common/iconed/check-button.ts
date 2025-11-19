34549a26880fa7119a6c3c9732cb4cdcc8cb2cec
rndviktor <rnd4job@gmail.com>
2025-11-19 11:39:47 +0100

    fix: fill="none" is deprecated

--- Teacher/FrontEnd/Teacher.Angular/src/app/components/common/iconed/check-button.ts
+++ Teacher/FrontEnd/Teacher.Angular/src/app/components/common/iconed/check-button.ts
@@ -8,7 +8,7 @@ import {Component, Input} from '@angular/core';
     [disabled]="disabled"
     type="submit"
   >
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill-opacity="0" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
     </svg>
   </button>`
